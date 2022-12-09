e8b35b9f4c5d1ae041103d34b3c980e03b6ee750
Mahmud Asrul <mahmud.asrul@ui.ac.id>
2022-12-09 18:42:49 +0700

    testing

--- lib/pages/courses/courses.dart
+++ lib/pages/courses/courses.dart
@@ -9,6 +9,34 @@ class Courses extends StatefulWidget {
 }
 
 class _CoursesState extends State<Courses> {
+
+  // @override
+  // void initState() {
+  //   Future<List<WatchList>> fetchWatchList() async {
+  //     var url =
+  //         Uri.parse('https://start-in.up.railway.app/courses/json');
+  //     var response = await http.get(
+  //       url,
+  //     );
+  //     var data = jsonDecode(utf8.decode(response.bodyBytes));
+  //     print(data);
+
+  //     List<WatchList> listToDo = [];
+  //     for (var d in data) {
+  //       if (d != null) {
+  //         listToDo.add(WatchList.fromJson(d));
+  //       }
+  //     }
+
+  //     return listToDo;
+  //   }
+
+  //   super.initState();
+  //   _remoteDataSource = WatchListRemoteDataSource();
+  //   print(_remoteDataSource);
+  //   _futureWatchList = _remoteDataSource.fetchWatchList();
+  // }
+  
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -40,7 +68,12 @@ class _CoursesState extends State<Courses> {
                     fontWeight: FontWeight.bold,
                     fontSize: 32.0)),
                   ),)
-            ]    
+              Container(width: double.infinity,
+              height: 200.0,
+              decoration:
+                const 
+              )
+            ]   
           ),
         ),
       ));
