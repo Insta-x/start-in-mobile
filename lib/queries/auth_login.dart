a4708949bcc054ce68d1cb74273abe84efba6ecb
Rhfialyndra <rahfialyendra5@gmail.com>
2022-12-10 17:18:11 +0700

    refactor

--- lib/queries/auth_login.dart
+++ lib/queries/auth_login.dart
@@ -1,11 +1,8 @@
 
 import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:flutter/material.dart';
 
 Future<bool> login(
     CookieRequest request, String username, String password) async {
-  print(request.cookies);
-  final response =
       await request.login('https://start-in.up.railway.app/auth/api/login/', {
     'username': username,
     'password': password,
