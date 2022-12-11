8391f13c43acaa4fdedd01fafea5bacff56362e2
Kohan5 <emrailatuconsina@gmail.com>
2022-12-11 15:34:35 +0700

    realisasi mimpi-mimpi

--- lib/queries/get_all_news.dart
+++ lib/queries/get_all_news.dart
@@ -15,7 +15,7 @@ Future<List<News>> get_all_news() async {
         // melakukan decode response menjadi bentuk json
         var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-        // melakukan konversi data json menjadi object Forum
+        // melakukan konversi data json menjadi object News
         List<News> news = [];
         for (var d in data) {
         if (d != null) {
