0eca99b254aa70c52fd762a8e073d2584bfdcde0
Rafi Rasendrya Favian <rafi.rasendrya@ui.ac.id>
2022-12-11 13:50:15 +0700

    Finish get json event and design event.dart

--- lib/pages/events/events.dart
+++ lib/pages/events/events.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:start_in_mobile/queries/get_all_events.dart';
 import 'package:start_in_mobile/widgets/drawer.dart';
+import 'package:start_in_mobile/pages/events/eventsDetail.dart';
 
 class Events extends StatefulWidget {
   const Events({super.key});
@@ -15,37 +17,92 @@ class _EventsState extends State<Events> {
       appBar: AppBar(
         title: const Text("Events"),
         backgroundColor: Color.fromARGB(255, 69, 39, 77),
-
       ),
       drawer: const AppDrawer(),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: Column(
-              children: <Widget>[
-                // Image.asset('assets/image1.jpg'),
-                Text('Card 1 - Talkshow'),
-              ],
-            ),
-          ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                // Image.asset('assets/image2.jpg'),
-                Text('Card 2 - Workshop'),
-              ],
-            ),
-          ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                // Image.asset('assets/image3.jpg'),
-                Text('Card 3 - Talkshow'),
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: FutureBuilder(
+        future: get_all_events(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData) {
+              return Column(
+                children: const [
+                  Text(
+                    "Tidak ada events :(",
+                    style:
+                        TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) => Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16, vertical: 12),
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.black, blurRadius: 2.0)
+                    ]
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Tipe event --> Talkshow/Workshop
+                      Text(
+                        "${snapshot.data![index].fields.event_type}",
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      // Judul Event
+                      Text(
+                        "${snapshot.data![index].fields.event_title}",
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // Jarak line spacing
+                      Text(
+                        "",
+                        style: const TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      // Jadwal event
+                      Text(
+                        "${snapshot.data![index].fields.schedule}",
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      // Lokasi event
+                      Text(
+                        "${snapshot.data![index].fields.location}",
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              );
+            }
+          }
+        }
+      )
     );
   }
 }
