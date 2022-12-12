7ffc57449fee5f1f6e5e8f96a55c4e538782c8d7
Insta-x <uttsadajason@outlook.com>
2022-12-12 19:20:41 +0700

    fix project edit

--- lib/pages/projects/edit_project.dart
+++ lib/pages/projects/edit_project.dart
@@ -41,9 +41,9 @@ class _EditProjectPageState extends State<EditProjectPage> {
         future: fetchProject(request, widget.projectId),
         builder: (BuildContext context, AsyncSnapshot<Project> snapshot) {
           if (snapshot.hasData) {
-            title = snapshot.data!.title;
-            description = snapshot.data!.description;
-            donationTarget = snapshot.data!.donationTarget;
+            print(title);
+            print(description);
+            print(donationTarget);
 
             return Form(
               key: _editProjectFormKey,
@@ -65,9 +65,9 @@ class _EditProjectPageState extends State<EditProjectPage> {
                           ),
                         ),
                         // Menambahkan behavior saat nama diketik
-                        onChanged: (String? value) {
-                          title = value!;
-                        },
+                        // onChanged: (String? value) {
+                        //   title = value!;
+                        // },
                         // Menambahkan behavior saat data disimpan
                         onSaved: (String? value) {
                           title = value!;
@@ -97,9 +97,9 @@ class _EditProjectPageState extends State<EditProjectPage> {
                           ),
                         ),
                         // Menambahkan behavior saat nama diketik
-                        onChanged: (String? value) {
-                          description = value!;
-                        },
+                        // onChanged: (String? value) {
+                        //   description = value!;
+                        // },
                         // Menambahkan behavior saat data disimpan
                         onSaved: (String? value) {
                           description = value!;
@@ -132,11 +132,11 @@ class _EditProjectPageState extends State<EditProjectPage> {
                           FilteringTextInputFormatter.digitsOnly
                         ],
                         // Menambahkan behavior saat nama diketik
-                        onChanged: (String? value) {
-                          if (int.tryParse(value!) != null) {
-                            donationTarget = int.parse(value);
-                          }
-                        },
+                        // onChanged: (String? value) {
+                        //   if (int.tryParse(value!) != null) {
+                        //     donationTarget = int.parse(value);
+                        //   }
+                        // },
                         // Menambahkan behavior saat data disimpan
                         onSaved: (String? value) {
                           if (int.tryParse(value!) != null) {
@@ -163,6 +163,7 @@ class _EditProjectPageState extends State<EditProjectPage> {
                           ),
                           onPressed: () async {
                             if (_editProjectFormKey.currentState!.validate()) {
+                              _editProjectFormKey.currentState!.save();
                               await editProject(request, widget.projectId,
                                       title, description, donationTarget)
                                   .then((value) => Navigator.pop(context));
